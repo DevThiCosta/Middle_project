7a5c90eb3f725d6ebc8e8462e0702e2a7b9a3b89
Thiago Costa <dev_thiscosta@outlook.com>
2024-05-17 22:46:00 -0700

    fix some responsive issues, doing som,e changes on home page and building the users page

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,10 +1,19 @@
-// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+// Import dependencies and configure Stimulus
+import { Application } from "@hotwired/stimulus"
 import "@hotwired/turbo-rails"
-import "controllers"
 import "@popperjs/core"
 import "bootstrap"
-import { Application } from "@hotwired/stimulus"
-window.Stimulus = Application.start()
 
-import FilterCardsController from "./controllers/disable_button_controller.js"
+// Import and register Stimulus controllers
+import FilterCardsController from "./controllers/filter_cards_controller.js"
+import TogglePasswordController from "./controllers/toggle_password_controller.js"
+import DatepickerController from "./controllers/datepicker_controller.js"
+import TabsController from "./controllers/tabs_controller.js"
+import HelloController from "./controllers/hello_controller.js"
+
+window.Stimulus = Application.start()
 Stimulus.register("filter-cards", FilterCardsController)
+Stimulus.register("toggle-password", TogglePasswordController)
+Stimulus.register("datepicker", DatepickerController)
+Stimulus.register("tabs", TabsController)
+Stimulus.register("hello", HelloController)
